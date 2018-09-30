48f7833ea98f1bebe534f95df542978e814ca1f9
neikvon <neikvon@hotmail.com>
2018-09-30 17:49:09 +0800

    aaa2

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ console.log(123456)
 console.log(`don't install it. it's just a demo for fbi-commit!`)
 
 console.log(2)
+console.log(`don't install it. it's just a demo for fbi-commit!`)
+
+console.log('add another line')
+
+console.log('')
